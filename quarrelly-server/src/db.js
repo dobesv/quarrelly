a5b0dd7b484025ea808026027c7431ef95819fc7
Dobes Vandermeer <dobesv@gmail.com>
2017-06-29 15:05:24 -0700

    More sophisticated use of mongodb

--- quarrelly-server/src/db.js
+++ quarrelly-server/src/db.js
@@ -1,64 +1,50 @@
 const MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/quarrelly';
-
-
-// In-memory database for now
-const db = {
-    topics: [{
-        id: "t1",
-        title: 'iOS or Android?',
-        creator: 'dobesv@gmail.com',
-        created: new Date(),
-        modified: new Date(),
-        comments: [{
-            id: 1,
-            created: new Date(),
-            modified: new Date(),
-            userId: 'dobesv@gmail.com',
-            text: 'Android, I guess.  I like it better, anyway.'
-        }]
-    }]
-};
-
 var connection = MongoClient.connect(url);
 var topicsPromise = connection.then((db) => db.collection('topics'));
 
 
 // List all topics, including all comments recursively
-exports.listTopics = () => topicsPromise.then((topics) => 
-    topics.find({}).toArray());
+exports.listTopics = async () => (await topicsPromise).find({}).toArray();
 
 // Get a topic by id, or null if none exists with that id
-exports.getTopicById = (id) => topicsPromise.then((topics) => 
-    topics.findOne({id: id}));
+exports.getTopicById = async (id) => (await topicsPromise).findOne({id: id});
 
 // Get a user's comment in a topic, or null if the user hasn't left a comment on that topic yet
-exports.getUserCommentForTopicId = (topicId, userId) => exports.getTopicById(topicId).then((topic) =>
-    topic && topic.comments.find((c) => c.userId === userId));
+exports.getUserCommentForTopicId = async (topicId, userId) => 
+    (await topicsPromise).findOne(
+        {id: topicId, "comments.userId": userId}, 
+        {'comments.$': true}
+    ).then((topic) => topic && topic.comments[0]);
 
 // Set a user's comment in a topic.  Creates a new topic if one didn't exist already
-exports.setUserCommentForTopicId = async (topicId, userId, text) =>  {
-    const topic = await exports.getTopicById(topicId)
-    if(!topic)
-        return null;
-    const comment = Object.assign({}, topic.comments.find((c) => c.userId == userId) ||
-        {
-            id: topicId+'#'+userId,
-            userId: userId,
-            created: new Date(),
+exports.setUserCommentForTopicId = (topicId, userId, text) => topicsPromise.then((topics) =>
+    topics.updateOne({
+        id: topicId,
+        "comments.userId": userId
+    },{
+        $set: {
+            "comments.$.text": text,
+            "comments.$.modified": new Date()
+        }
+    }).then((updateResult) => updateResult.modifiedCount === 0 ?
+        // If no comment existed before, add one instead
+        topics.updateOne({
+            id: topicId
         }, {
-            text: text,
-            modified: new Date()
-        });
-    const comments = [comment].concat(topic.comments.filter((c) => c.userId !== userId));
-    const topics = await topicsPromise;
-    const newTopic = Object.assign({}, topic, {
-        comments: comments,
-        modified: new Date()
-    });
-    const replaceResult = await topics.replaceOne({id: topicId}, newTopic);
-    return comment;
-};
+            // Add a new comment item to the end of the list
+            $push: {
+                comments: {
+                    id: topicId+'#'+userId,
+                    userId: userId,
+                    created: new Date(),
+                    text: text,
+                    modified: new Date()
+                }
+            }
+        }) : Promise.resolve(updateResult)
+    ).then((updateResult) => exports.getUserCommentForTopicId(topicId, userId))
+);
 
 // Add a new topic with the given title/question
 exports.addTopic = (userId, title) => topicsPromise.then(async (topics) => {
